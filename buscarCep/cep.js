e5a4aa511ec1727123853de89f3e467aee956da2
Bruno Caetano <brunocaetano403@gmail.com>
2024-12-05 13:43:11 -0300

    busca cep2

--- buscarCep/cep.js
+++ buscarCep/cep.js
@@ -66,3 +66,70 @@ else {
 }
 };
 
+
+// Função para limpar os campos do formulário de pesquisa
+function limpa_formulário_cep() {
+    document.getElementById('ruaSearch').value = "";
+    document.getElementById('bairroSearch').value = "";
+    document.getElementById('cidadeSearch').value = "";
+    document.getElementById('ufSearch').value = "";
+    document.getElementById('ibgeSearch').value = "";
+  }
+  
+  // Callback para processar a resposta da API
+  function meu_callback_pesquisa(conteudo) {
+    if (!("erro" in conteudo)) {
+      // Verifica se a cidade é do Paraná e se o nome da rua não está vazio
+      if (conteudo.uf === "PR" && conteudo.logradouro.trim() !== "") {
+        document.getElementById('ruaSearch').value = conteudo.logradouro;
+        document.getElementById('bairroSearch').value = conteudo.bairro;
+        document.getElementById('cidadeSearch').value = conteudo.localidade;
+        document.getElementById('ufSearch').value = conteudo.uf;
+        document.getElementById('ibgeSearch').value = conteudo.ibge;
+      } else {
+        alert("A pesquisa é válida apenas para cidades do Paraná (PR) com nome de rua especificado.");
+        limpa_formulário_cep();
+      }
+    } else {
+      limpa_formulário_cep();
+      alert("CEP não encontrado.");
+    }
+  }
+  
+function pesquisacep(valor) {
+    var cep = valor.replace(/\D/g, ''); // Remove caracteres não numéricos
+  
+    if (cep !== "") {
+      var validacep = /^[0-9]{8}$/; // Valida o formato do CEP
+      if (validacep.test(cep)) {
+        // Preenche os campos com "..." enquanto consulta a API
+        document.getElementById('ruaSearch').value = "...";
+        document.getElementById('bairroSearch').value = "...";
+        document.getElementById('cidadeSearch').value = "...";
+        document.getElementById('ufSearch').value = "...";
+        document.getElementById('ibgeSearch').value = "...";
+  
+        // Cria um elemento script para carregar a API do ViaCEP com a callback
+        var script = document.createElement('script');
+        script.src = `https://viacep.com.br/ws/${cep}/json/?callback=meu_callback_pesquisa`;
+        document.body.appendChild(script);
+      } else {
+        limpa_formulário_cep();
+        alert("Formato de CEP inválido.");
+      }
+    } else {
+      limpa_formulário_cep();
+    }
+  }
+  
+  // Função para formatar o CEP na entrada do usuário
+  function formatarCep(input) {
+    input.value = input.value.replace(/\D/g, '').replace(/(\d{5})(\d)/, '$1-$2');
+  }
+  
+  // Adiciona um evento de formatação ao input de CEP
+  window.onload = function() {
+    document.getElementById('cepSearchInput').addEventListener('input', function() {
+      formatarCep(this);
+    });
+  };
